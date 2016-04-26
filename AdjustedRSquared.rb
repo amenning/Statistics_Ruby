e5eedb4097d716ce40aac41effe804ee22d16fae
metakata <menning23@gmail.com>
2016-04-26 00:14:20 -0400

    update parameter validation

--- AdjustedRSquared.rb
+++ AdjustedRSquared.rb
@@ -18,26 +18,66 @@ class AdjustedRSquared
 		@r_square = r_square
 		@data_size = data_size
 		@num_regression_coefficients = num_regression_coefficients
+		update_adjustment(@r_square, @data_size, @num_regression_coefficients)
+	end
+	
+	def update_adjustment(r_square, data_size, num_regression_coefficients)
 		if validate_parameters
-			@adjusted_r_square = determine_adjustment(@r_square, @data_size, @num_regression_coefficients)
+			@adjusted_r_square = determine_adjustment(r_square, data_size, num_regression_coefficients)
 		else
 			# Raise error
 		end
 	end
 	
-	def update_adjustment(r_square, data_size, num_regression_coefficients)
-		@adjusted_r_square = determine_adjustment(r_square, data_size, num_regression_coefficients)
-	end
-	
 	private
 		def validate_parameters
-			if @data_size > @num_regression_coefficients
+			return false unless validate_parameters_type
+			return false unless validate_r_square_within_0_to_1
+			return false unless validate_data_size_greater_than_one
+			return false unless validate_num_regression_coefficients_greater_than_0
+			return false unless validate_data_size_greater_than_num_regression_coefficients
+			return true
+		end
+		
+		def validate_parameters_type
+			return false unless (@data_size.instance_of?(Fixnum) || @data_size.instance_of?(Bignum))
+			return false unless (@num_regression_coefficients.instance_of?(Fixnum) || @num_regression_coefficients.instance_of?(Bignum))
+			return false unless (@r_square.instance_of?(Float) || @r_square.instance_of?(Fixnum))
+			return true
+		end
+		
+		def validate_r_square_within_0_to_1
+			if (@r_square >= 0 && @r_square <= 1.0)
+				return true
+			else
+				return false
+			end
+		end
+
+		def validate_data_size_greater_than_one
+			if @data_size > 1
+				return true
+			else
+				return false
+			end
+		end				
+	
+		def validate_num_regression_coefficients_greater_than_0
+			if @num_regression_coefficients > 0
 				return true
 			else
 				return false
 			end
 		end
 		
+		def validate_data_size_greater_than_num_regression_coefficients
+			if @data_size > @num_regression_coefficients
+				return true
+			else
+				return false
+			end
+		end
+	
 		def determine_adjustment(r_square, data_size, num_regression_coefficients)
 			# R2adj = 1 - (1-(R2))(#datapoints - 1)/(#datapoints - #model_variables)
 			# Need to use 1.0 to ensure floating point division
--- AdjustedRSquared_spec.rb
+++ AdjustedRSquared_spec.rb
@@ -1,14 +1,50 @@
 require './AdjustedRSquared'
 
 describe AdjustedRSquared do
+	before(:all) do
+		@r_square = 0.9
+		@data_size = 50
+		@num_regression_coefficients = 5
+	end
+	
 	it "accepts three numbers upon initialization and provides adjusted r square" do
-		test = AdjustedRSquared.new(1,2,1)
+		test = AdjustedRSquared.new(@r_square,@data_size,@num_regression_coefficients)
 		expect(test.adjusted_r_square).not_to be_nil
 	end
 	
 	it "verifies that the data size is greater than the number of regression coefficients" do
-		test = AdjustedRSquared.new(1,1,1)
+		@data_size = 1
+		test = AdjustedRSquared.new(@r_square,@data_size,@num_regression_coefficients)
 		expect(test.adjusted_r_square).to be_nil
 	end
 
+	it "verifies that r_square is between 0 and 1" do
+		@r_square = 1.9
+		test = AdjustedRSquared.new(@r_square,@data_size,@num_regression_coefficients)
+		expect(test.adjusted_r_square).to be_nil
+	end
+	
+	it "verifies that input r_square is float or fixnum" do
+		@r_square = "A"
+		test = AdjustedRSquared.new(@r_square,@data_size,@num_regression_coefficients)
+		expect(test.adjusted_r_square).to be_nil
+	end
+
+	it "verifies that input data_size is fixnum or bignum" do
+		@data_size = "A"
+		test = AdjustedRSquared.new(@r_square,@data_size,@num_regression_coefficients)
+		expect(test.adjusted_r_square).to be_nil
+	end
+
+	it "verifies that input num_regression_coefficients is fixnum or bignum" do
+		@num_regression_coefficients = "A"
+		test = AdjustedRSquared.new(@r_square,@data_size,@num_regression_coefficients)
+		expect(test.adjusted_r_square).to be_nil
+	end
+
+	it "verifies that input num_regression_coefficients is greater than 0" do
+		@num_regression_coefficients = 0
+		test = AdjustedRSquared.new(@r_square,@data_size,@num_regression_coefficients)
+		expect(test.adjusted_r_square).to be_nil
+	end		
 end
